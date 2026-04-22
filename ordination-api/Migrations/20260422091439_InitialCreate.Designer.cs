225e142a15f38171b26fd84e22ef9727891556fe
selale920 <selaleg8@gmail.com>
2026-04-22 11:17:00 +0200

    fix

--- ordination-api/Migrations/20260422091439_InitialCreate.Designer.cs
+++ ordination-api/Migrations/20260422091439_InitialCreate.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -10,9 +11,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace ordination_api.Migrations
 {
     [DbContext(typeof(OrdinationContext))]
-    partial class OrdinationContextModelSnapshot : ModelSnapshot
+    [Migration("20260422091439_InitialCreate")]
+    partial class InitialCreate
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder.HasAnnotation("ProductVersion", "9.0.8");
